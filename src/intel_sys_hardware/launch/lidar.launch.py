9141d628076ed50da2e1eb1429d00e3d6dbcb60d
Aahil Shaikh <aahil.aref@gmail.com>
2026-09-17 12:28:11 -0700

    fixed unitree frame issues

--- src/intel_sys_hardware/launch/lidar.launch.py
+++ src/intel_sys_hardware/launch/lidar.launch.py
@@ -6,14 +6,14 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('lidar_ip', default_value='192.168.1.2', description='Unitree LiDAR IP address'),
-        DeclareLaunchArgument('frame_id', default_value='lidar_link', description='LiDAR frame ID'),
         Node(
             package='unitree_lidar_ros2',
             executable='unitree_lidar_ros2_node',
             name='unitree_lidar_ros2_node',
             output='screen',
             parameters=[{
-                'frame_id': LaunchConfiguration('frame_id'),
+                'cloud_frame': 'lidar_link',
+                'imu_frame': 'lidar_imu_link',
             }],
             remappings=[
                 ('/unilidar/cloud', '/lidar/points_raw'),
